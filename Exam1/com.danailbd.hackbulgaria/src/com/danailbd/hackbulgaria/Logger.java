2c8907eb481c22f3b3cf7b292077c0cb4128bb91
danailbd <danailbd@localhost.localdomain>
2014-06-19 11:03:54 +0300

    Added exam1 files and some implementation

---
+++ Exam1/com.danailbd.hackbulgaria/src/com/danailbd/hackbulgaria/Logger.java
@@ -0,0 +1,39 @@
+package com.danailbd.hackbulgaria;
+
+import java.io.Console;
+
+public class Logger {
+	
+	private int LEVEL;
+
+	public Logger() {
+		LEVEL = 3;
+	}
+	
+	public Logger(int lEVEL) {
+		LEVEL = lEVEL;
+	}
+
+	public void setLevel(int lEVEL) {
+		LEVEL = lEVEL;
+	}
+	
+	
+	public void	log(int level, String message) {
+		
+		if(level > 0){
+			
+			if(level < LEVEL)
+				System.out.print(level + " => " + message);
+		}
+		else{
+			System.out.println("Bad input");
+		}
+	}
+
+	public void	log(String message) {
+		
+		log(3, message);
+	}
+	
+}

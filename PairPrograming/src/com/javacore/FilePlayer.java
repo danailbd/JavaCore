9d330f783c9dbea398da4efea928abb28dfebfd3
edeyanova <deyanovae@aol.com>
2014-07-10 18:57:44 +0300

    Quich fix

--- PairPrograming/src/com/javacore/FilePlayer.java
+++ PairPrograming/src/com/javacore/FilePlayer.java
@@ -1,5 +1,6 @@
 package com.javacore;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 public abstract class FilePlayer implements com.javacore.Player {
@@ -7,6 +8,7 @@ public abstract class FilePlayer implements com.javacore.Player {
 	public static int consoleSize = 100;
 	public static int hIndex = 2; // sets height reduce index of a pixel
 	public File file;
+	protected abstract BufferedImage getNextFrame();
 
 	public FilePlayer(String fileToPlay) {
 		file = new File(fileToPlay);
--- PairPrograming/src/com/javacore/GifPlayer.java
+++ PairPrograming/src/com/javacore/GifPlayer.java
@@ -6,9 +6,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class GifPlayer extends com.javacore.FilePlayer {
-
+	 private GifDecoder decoder;
+	 private int lastFrameIndex;
+	 
 	public GifPlayer(String fileToPlay) {
 		super(fileToPlay);
+		this.decoder = new GifDecoder();
+	    this.lastFrameIndex = 0;
+
 	}
 
 	@Override
@@ -16,38 +21,15 @@ public class GifPlayer extends com.javacore.FilePlayer {
 		// TODO Auto-generated method stub
 		
 	}
-	protected int status;
-	protected int FrameCount;
-	public BufferedImage image;
-	protected ArrayList frames;
-	protected BufferedInputStream in;
-	public void read(BufferedInputStream fileToPlay){
-		if(fileToPlay!=null){
-			 if (!(fileToPlay instanceof BufferedInputStream))
-			        fileToPlay = new BufferedInputStream(fileToPlay);
-			 		in = (BufferedInputStream) fileToPlay;
-			 		
 
-			 		
-			 		
-		}
-		try {
-		      fileToPlay.close();
-		    } catch (IOException e) {
-		    }
-	}
-	
 
-	public BufferedImage getFrame(int n){
-		BufferedImage im = null;
-	    if ((n >= 0) && (n < FrameCount)) {
-	      im = ((GifPlayer) frames.get(n)).image;
-	    }
-	    return im;
-	}
-	public int getFrameCount(){
-		return FrameCount;
-	}
+    @Override
+    protected BufferedImage getNextFrame() {
+        BufferedImage frame = decoder.getFrame(lastFrameIndex);
+        lastFrameIndex++;
+        return frame;
+    }
+
 	
 	
 

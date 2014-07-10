4f0043b4ebb6db6b19828423dfe2f3ef9070392a
danailbd <danailbd@localhost.localdomain>
2014-07-10 20:57:34 +0300

    Added some functionality

---
+++ TicTacToe/src/com/danailbd/CellAlreadyTakeException.java
@@ -0,0 +1,33 @@
+package com.danailbd;
+
+class CellAlreadyTakeException extends Exception {
+
+	private int x, y;
+
+	public CellAlreadyTakeException(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String getMessage() {
+		return "Cell already taken";
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}

989bdb3af8d7d8b4735b2069f08423d6d541313b
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-07-17 15:36:44 +0300

    add

--- RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
+++ RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
@@ -6,13 +6,14 @@ public class Protocol {
 	
 	private static final String EOM = "<[EOM]>";
 	private static final int PORT = 3000;
-	private static String serverIP;
 	
+
     public static void readRBImessage(Socket socket) {
 		
 	}
 	
     public static void writeRBImessage(Socket socket) {
+
     }
 	
 }

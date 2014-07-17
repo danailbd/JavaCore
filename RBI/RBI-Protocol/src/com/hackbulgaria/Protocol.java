4733deed8e4870f933615394e9cb6a8f157cef18
danailbd <danailbd@localhost.localdomain>
2014-07-17 15:30:38 +0300

    Added main structute for protocol

--- RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
+++ RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
@@ -1,5 +1,15 @@
 package com.hackbulgaria;
 
 public class Protocol {
-
+	
+	private static final String EOM = "<[EOM]>";
+	private static final int PORT = 3000;
+	private static String serverIP;
+	
+	public static void readRBImessage(Socket ){
+		
+	}
+	
+	public static void writeRBImessage(Socket ){}
+	
 }

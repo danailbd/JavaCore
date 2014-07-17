b74f308f42c058ff37e4a76249848547514c2116
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-07-17 16:03:35 +0300

    Add read function

--- RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
+++ RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
@@ -1,14 +1,14 @@
 package com.hackbulgaria;
-
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Protocol {
 	
 	private static final String EOM = "<[EOM]>";
-	private static final int PORT = 3000;
+	public static final int PORT = 3000;
 	
 
     public static String readRBImessage(Socket clientSocket) throws IOException {
@@ -28,6 +28,14 @@ public class Protocol {
 	
     public static void writeRBImessage(Socket socket, String message) {
     	
+    	PrintWriter outStream = null;
+		try {
+			outStream = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+    	outStream.println(message + EOM);
     }
 	
 }

716aef9d528485c471d11f8888c632b6cc019197
danailbd <danailbd@localhost.localdomain>
2014-07-17 15:57:14 +0300

    Added write method implementation for protocol

--- RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
+++ RBI/RBI-Protocol/src/com/hackbulgaria/Protocol.java
@@ -1,11 +1,13 @@
 package com.hackbulgaria;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Protocol {
 	
 	private static final String EOM = "<[EOM]>";
-	private static final int PORT = 3000;
+	public static final int PORT = 3000;
 	
 
     public static void readRBImessage(Socket socket) {
@@ -14,6 +16,14 @@ public class Protocol {
 	
     public static void writeRBImessage(Socket socket, String message) {
     	
+    	PrintWriter outStream = null;
+		try {
+			outStream = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+    	outStream.println(message + EOM);
     }
 	
 }

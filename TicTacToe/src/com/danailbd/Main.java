2e00c76c3200c63c777609a1fd08ba9ca2865373
danailbd <danailbd@localhost.localdomain>
2014-07-24 12:38:01 +0300

    Quick fix

--- TicTacToe/src/com/danailbd/Main.java
+++ TicTacToe/src/com/danailbd/Main.java
@@ -1,21 +1,17 @@
 package com.danailbd;
 
 import java.io.IOException;
-import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Stack<String> x = new Stack<>();
-		x.pop();
 		
-//		GameUserInput game = new GameUserInput();
-//		try {
-//			game.runGame();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		GameUserInput game = new GameUserInput();
+		try {
+			game.runGame();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
